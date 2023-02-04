bfbf6c5be5cbc1472a09d50f52520fa33becd1dc
GOpinathKgn <gopikaruppasamy@gmail.com>
2023-02-04 22:26:40 +0530

    Vigneshgit

--- src/Org/lang/LanguageInfo.java
+++ src/Org/lang/LanguageInfo.java
@@ -2,26 +2,41 @@ package Org.lang;
 
 public class LanguageInfo {
 
-private void tamil() {
-	System.out.println("Tamil Language : Tamilnadu");
+	private void tamil() {
+		System.out.println("Tamil Language : Tamilnadu");
 
-}
+	}
+
 	private void hindi() {
 		System.out.println("Hindi Language : New Delhi");
 	}
-	
+
 	private void english() {
 		System.out.println("English Language : USA");
 
 	}
+
+	private void kannadam() {
+		System.out.println("Kannadam Language : Karnataka ");
+
+	}
+
+	private void Malayalam() {
+		System.out.println("Malayalam Language: Kerala");
+
+	}
+
 	public static void main(String[] args) {
-		LanguageInfo l=new LanguageInfo();
-				l.tamil();
+		LanguageInfo l = new LanguageInfo();
+		l.tamil();
 		l.hindi();
 		l.english();
-		StateDetails s=new StateDetails();
+		l.Malayalam();
+		l.kannadam();
+
+		StateDetails s = new StateDetails();
 		s.south();
 		s.north();
-		}
-	
+	}
+
 }
